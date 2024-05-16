f8e4fb1621070839f10f43fe910478678ebc74fc
Lipe <silvafelipes021@gmail.com>
2024-05-15 22:01:56 -0300

    update

--- tweet/urls.py
+++ tweet/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import TweetViewSet
 
 urlpatterns = [
-    path('tweets/', TweetViewSet.as_view({'get': 'list', 'post': 'create'}), name='tweet-list'),  # Listar tweets e criar novos tweets
-    path('tweets/<int:pk>/', TweetViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='tweet-detail'),  # Detalhes, atualizar e deletar tweet específico
+    path('tweets/', TweetViewSet.as_view({'get': 'list', 'post': 'create'}), name='tweet-list'),  
+    path('tweets/<int:pk>', TweetViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='tweet-detail'),  
 ]
